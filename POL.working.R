2118d01e24a40d68bef94635fb8131d09e7ee328
Philip Waggoner <31326382+pdwaggoner@users.noreply.github.com>
2018-04-10 14:23:04 -0500

    Update POL.working.R

--- POL.working.R
+++ POL.working.R
@@ -18,7 +18,6 @@ experiment2 <- read.csv(".../Experiment 2/results01112018.csv", as.is = TRUE)
 experiment3 <- read.csv(".../Experiment 3/results01112018.csv", as.is = TRUE)
 experiment4 <- read.csv(".../Experiment 4/results03052018.csv", as.is = TRUE)
 
-
 ####
 ####
 #### Create some variables in similar syntax as CJ Exp
